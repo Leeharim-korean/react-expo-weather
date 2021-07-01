dd395cf8dd584f737c46e514b2a8903675fd1da3
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2021-07-01 15:26:10 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = { 
+    isLoading: true
+  };
   getLoctaion = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("GPS turns off", "Can't find you");
+    }
   };
   componentDidMount() {
     this.getLoctaion();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
   }
 }
