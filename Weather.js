c8710cc7eab73549e49e4f967ec99405eff61d9d
Leeharim-korean <67570225+Leeharim-korean@users.noreply.github.com>
2021-07-01 19:18:10 +0900

    #2.1 Getting the Condition Names

--- Weather.js
+++ Weather.js
@@ -11,7 +11,23 @@ export default function Weather({ temp }) {
 }
 
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Mist",
+        "Smoke",
+        "Haze",
+        "Dust",
+        "Sand",
+        "Ash",
+        "Squall",
+        "Tornado",
+        "Clear",
+        "Clouds"
+    ]).isRequired
 };
 
 const styles = StyleSheet.create({
